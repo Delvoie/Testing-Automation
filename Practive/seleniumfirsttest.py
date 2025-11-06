3c4ab99bf267d1cf0082a02e3b5f6b3743362b1c
LucasdoesCode <arozegames@gmail.com>
2025-11-06 18:43:22 -0500

    s

--- Practive/seleniumfirsttest.py
+++ Practive/seleniumfirsttest.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.get("https://ai.google.dev/")
+
+driver = webdriver.Firefox()
+driver.get("https://www.wikipedia.org/")
 print(driver.title)
 
+searchbox = driver.find_element(By.ID, "searchInput")
+searchbox.send_keys("Selenium (software)")
+driver.implicitly_wait(3)
+searchbox.click()
+driver.implicitly_wait(3)
+
+print(driver.title)
 driver.quit()

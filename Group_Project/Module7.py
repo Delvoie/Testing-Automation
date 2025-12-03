ca032506f43d1e8d8b0182f3e2b793c04a11d566
Lucas Delvoie <arozegames@gmail.com>
2025-12-03 15:39:56 -0500

    add comments

--- Group_Project/Module7.py
+++ Group_Project/Module7.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-class Lab4TestCase(unittest.TestCase):
+class Module7TestCases(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -22,11 +22,14 @@ class Lab4TestCase(unittest.TestCase):
 
     def setUp(self):
         self.driver.get("http://demo.guru99.com/V4/")
-        self.driver.find_element(By.NAME, "uid").send_keys("mngr647644")
+        self.driver.find_element(By.NAME, "uid").send_keys("mngr647644") # Lucas' id and password
         self.driver.find_element(By.NAME, "password").send_keys("tyvAseg")
         self.driver.find_element(By.NAME, "btnLogin").click()
         time.sleep(2) # wait for login
 
+    # Test Cases for Balance Enquiry
+
+    # Test Case BE1: Verify that an error message is displayed when the Account Number field is left blank.
     def test_BE1(self):
         element = self.driver.find_element(By.LINK_TEXT, "Balance Enquiry")
         self.driver.execute_script("arguments[0].click();", element)
@@ -34,6 +37,7 @@ class Lab4TestCase(unittest.TestCase):
         self.driver.find_element(By.NAME, "accountno").send_keys(Keys.TAB)
         self.assertEqual(self.driver.find_element(By.ID, "message2").text, "Account Number must not be blank")
         print("Test BE1 passed")
+    # Test Case BE2: Verify that an error message is displayed when the Account Number field contains alphabetic characters.
     def test_BE2(self):
         element = self.driver.find_element(By.LINK_TEXT, "Balance Enquiry")
         self.driver.execute_script("arguments[0].click();", element)
@@ -41,6 +45,7 @@ class Lab4TestCase(unittest.TestCase):
         self.driver.find_element(By.NAME, "accountno").send_keys("1234Acc123")
         self.assertEqual(self.driver.find_element(By.ID, "message2").text, "Characters are not allowed")
         print("Test BE2 passed")
+    # Test Case BE3: Verify that an error message is displayed when the Account Number field contains special characters.
     def test_BE3(self):
         element = self.driver.find_element(By.LINK_TEXT, "Balance Enquiry")
         self.driver.execute_script("arguments[0].click();", element)
@@ -48,6 +53,7 @@ class Lab4TestCase(unittest.TestCase):
         self.driver.find_element(By.NAME, "accountno").send_keys("23!@#!@#")
         self.assertEqual(self.driver.find_element(By.ID, "message2").text, "Special characters are not allowed")
         print("Test BE3 passed")
+    # Test Case BE4: Verify that an error message is displayed when the Account Number field contains leading spaces.
     def test_BE4(self):
         element = self.driver.find_element(By.LINK_TEXT, "Balance Enquiry")
         self.driver.execute_script("arguments[0].click();", element)
